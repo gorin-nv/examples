6d27a1e12ac8eb69a9714d606f48fc3506dfcfd7
gorin-nv <gorin.nv@gmail.com>
2013-03-12 23:27:57 +0600

    [+] add test for MakeDiff [*] refactoring

--- UnitTests/DiffMergeTests.cs
+++ UnitTests/DiffMergeTests.cs
@@ -6,38 +6,62 @@ using NUnit.Framework;
 
 namespace UnitTests
 {
-    public interface ISubstringPosition
+    public class CommonPart
     {
-        int FirstStart { get; }
-        int SecondStart { get; }
-        string Value { get; }
-    }
-
-    public class SubstringPosition : ISubstringPosition
-    {
-        private readonly string _firstSource;
-        private readonly int _length;
-
-        public SubstringPosition(int firstStart, int secondStart, int length, string firstSource)
+        public CommonPart(int firstStart, int secondStart, int length)
         {
-            _firstSource = firstSource;
-            _length = length;
-            SecondStart = secondStart;
             FirstStart = firstStart;
+            SecondStart = secondStart;
+            Length = length;
         }
 
         public int FirstStart { get; private set; }
         public int SecondStart { get; private set; }
-
-        public string Value
-        {
-            get { return _firstSource.Substring(FirstStart, _length); }
-        }
+        public int Length { get; private set; }
     }
 
-    public static class TextCompareUtil
+    public class DifferencePart
     {
-        public static IEnumerable<ISubstringPosition> CommonSubstrings(string text1, string text2)
+        public DifferencePart(int firstStart, int firstLength, int secondStart, int secondLength)
+        {
+            FirstStart = firstStart;
+            FirstLength = firstLength;
+            SecondStart = secondStart;
+            SecondLength = secondLength;
+        }
+
+        public int FirstStart { get; private set; }
+        public int FirstLength { get; private set; }
+        public int SecondStart { get; private set; }
+        public int SecondLength { get; private set; }
+    }
+
+    public class Diff
+    {
+        public Diff(IEnumerable<DifferencePart> differenceParts, IEnumerable<CommonPart> identicalParts)
+        {
+            CommonParts = identicalParts;
+            DifferenceParts = differenceParts;
+        }
+
+        public IEnumerable<DifferencePart> DifferenceParts { get; private set; }
+        public IEnumerable<CommonPart> CommonParts { get; private set; }
+    }
+
+    public static class DiffMergeUtil
+    {
+        public static Diff MakeDiff(string text1, string text2)
+        {
+            var positions = CommonSubstrings(text1, text2);
+            return MakeDiff(positions, text1.Length, text2.Length);
+        }
+
+        public static Diff MakeDiff(IEnumerable<CommonPart> substringPositions, int firstLength, int secondLength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<CommonPart> CommonSubstrings(string text1, string text2)
         {
             var currentFirstPosition = 0;
             var currentSecondPosition = 0;
@@ -49,12 +73,12 @@ namespace UnitTests
                 if (position == null)
                     yield break;
                 yield return position;
-                currentFirstPosition = position.FirstStart + position.Value.Length;
-                currentSecondPosition = position.SecondStart + position.Value.Length;
+                currentFirstPosition = position.FirstStart + position.Length;
+                currentSecondPosition = position.SecondStart + position.Length;
             }
         }
 
-        public static SubstringPosition FindFirstSubstribngPosition(string text1, int position1, string text2, int position2)
+        public static CommonPart FindFirstSubstribngPosition(string text1, int position1, string text2, int position2)
         {
             int start1;
             int start2;
@@ -69,7 +93,7 @@ namespace UnitTests
                 text1, start1,
                 text2, start2);
 
-            return new SubstringPosition(start1, start2, length, text1);
+            return new CommonPart(start1, start2, length);
         }
 
         public static bool FindFirstCommonChar(string text1, int position1, string text2, int position2, out int start1, out int start2)
@@ -105,78 +129,117 @@ namespace UnitTests
     }
 
     [TestFixture]
-    public class TextCompareUtilTests
+    public class DiffMergeUtilTests
     {
+        [Test]
+        public void MakeDiff_should_make_diff()
+        {
+            var diff = DiffMergeUtil.MakeDiff(
+                new[]
+                    {
+                        new CommonPart(1, 2, 4),
+                        new CommonPart(7, 9, 2)
+                    },
+                15, 20);
+
+            diff.DifferenceParts.Should().HaveCount(3);
+
+            diff.DifferenceParts.ElementAt(0).FirstStart.Should().Be(0);
+            diff.DifferenceParts.ElementAt(0).FirstLength.Should().Be(1);
+            diff.DifferenceParts.ElementAt(0).SecondStart.Should().Be(0);
+            diff.DifferenceParts.ElementAt(0).SecondLength.Should().Be(2);
+
+            diff.DifferenceParts.ElementAt(1).FirstStart.Should().Be(5);
+            diff.DifferenceParts.ElementAt(1).FirstLength.Should().Be(2);
+            diff.DifferenceParts.ElementAt(1).SecondStart.Should().Be(6);
+            diff.DifferenceParts.ElementAt(1).SecondLength.Should().Be(3);
+
+            diff.DifferenceParts.ElementAt(2).FirstStart.Should().Be(9);
+            diff.DifferenceParts.ElementAt(2).FirstLength.Should().Be(6);
+            diff.DifferenceParts.ElementAt(2).SecondStart.Should().Be(11);
+            diff.DifferenceParts.ElementAt(2).SecondLength.Should().Be(9);
+
+            diff.CommonParts.Should().HaveCount(2);
+
+            diff.CommonParts.ElementAt(0).FirstStart.Should().Be(1);
+            diff.CommonParts.ElementAt(0).SecondStart.Should().Be(2);
+            diff.CommonParts.ElementAt(0).Length.Should().Be(4);
+
+            diff.CommonParts.ElementAt(0).FirstStart.Should().Be(7);
+            diff.CommonParts.ElementAt(0).SecondStart.Should().Be(9);
+            diff.CommonParts.ElementAt(0).Length.Should().Be(2);
+        }
+
         [Test]
         public void CommonSubstrings_should_find_common_substrings()
         {
             var text1 = "123abc789def";
             var text2 = "012a3c78ghe9iej";
 
-            var substrings = TextCompareUtil.CommonSubstrings(text1, text2);
+            var substrings = DiffMergeUtil.CommonSubstrings(text1, text2);
 
             substrings.Should().HaveCount(5);
 
             substrings.ElementAt(0).FirstStart.Should().Be(0);
             substrings.ElementAt(0).SecondStart.Should().Be(1);
-            substrings.ElementAt(0).Value.Should().Be("12");
+            substrings.ElementAt(0).Length.Should().Be(2);
 
             substrings.ElementAt(1).FirstStart.Should().Be(2);
             substrings.ElementAt(1).SecondStart.Should().Be(4);
-            substrings.ElementAt(1).Value.Should().Be("3");
+            substrings.ElementAt(1).Length.Should().Be(1);
 
             substrings.ElementAt(2).FirstStart.Should().Be(5);
             substrings.ElementAt(2).SecondStart.Should().Be(5);
-            substrings.ElementAt(2).Value.Should().Be("c78");
+            substrings.ElementAt(2).Length.Should().Be(3);
 
             substrings.ElementAt(3).FirstStart.Should().Be(8);
             substrings.ElementAt(3).SecondStart.Should().Be(11);
-            substrings.ElementAt(3).Value.Should().Be("9");
+            substrings.ElementAt(3).Length.Should().Be(1);
 
             substrings.ElementAt(4).FirstStart.Should().Be(10);
             substrings.ElementAt(4).SecondStart.Should().Be(13);
-            substrings.ElementAt(4).Value.Should().Be("e");
+            substrings.ElementAt(4).Length.Should().Be(1);
         }
 
         [Test]
         public void FindFirstSubstribngPosition_find_first_position()
         {
-            var position1 = TextCompareUtil.FindFirstSubstribngPosition(
+            var position1 = DiffMergeUtil.FindFirstSubstribngPosition(
                 "abc123abc456", 0,
                 "qwebbccbce", 0);
             position1.FirstStart.Should().Be(1);
             position1.SecondStart.Should().Be(3);
-            position1.Value.Should().Be("b");
+            position1.Length.Should().Be(1);
 
-            var position2 = TextCompareUtil.FindFirstSubstribngPosition(
+            var position2 = DiffMergeUtil.FindFirstSubstribngPosition(
                 "abc123abc456", 0,
                 "qwebbccbce", 5);
             position2.FirstStart.Should().Be(1);
             position2.SecondStart.Should().Be(7);
-            position2.Value.Should().Be("bc");
+            position2.Length.Should().Be(2);
 
-            var position3 = TextCompareUtil.FindFirstSubstribngPosition(
+            var position3 = DiffMergeUtil.FindFirstSubstribngPosition(
                 "abc123abc456", 2,
                 "qwebbccbce", 1);
             position3.FirstStart.Should().Be(2);
             position3.SecondStart.Should().Be(5);
-            position3.Value.Should().Be("c");
+            position3.Length.Should().Be(1);
 
-            var position4 = TextCompareUtil.FindFirstSubstribngPosition(
+            var position4 = DiffMergeUtil.FindFirstSubstribngPosition(
                 "abc123abc456", 2,
                 "qwebbccbce", 1);
             position4.FirstStart.Should().Be(2);
             position4.SecondStart.Should().Be(5);
-            position4.Value.Should().Be("c");
+            position4.Length.Should().Be(1);
 
-            var position5 = TextCompareUtil.FindFirstSubstribngPosition(
+            var position5 = DiffMergeUtil.FindFirstSubstribngPosition(
                 "abc123abc456", 3,
                 "qwebbccbce", 5);
             position5.FirstStart.Should().Be(7);
             position5.SecondStart.Should().Be(7);
-            position5.Value.Should().Be("bc");
+            position5.Length.Should().Be(2);
 
-            var position6 = TextCompareUtil.FindFirstSubstribngPosition(
+            var position6 = DiffMergeUtil.FindFirstSubstribngPosition(
                 "abc123abc456", 9,
                 "qwebbccbce", 1);
             position6.Should().BeNull();
@@ -187,103 +250,49 @@ namespace UnitTests
         {
             int start1, start2;
 
-            TextCompareUtil.FindFirstCommonChar(
+            DiffMergeUtil.FindFirstCommonChar(
                 "012345678", 1,
                 "_2____2__", 0,
                 out start1, out start2).Should().BeTrue();
             start1.Should().Be(2);
             start2.Should().Be(1);
 
-            TextCompareUtil.FindFirstCommonChar(
+            DiffMergeUtil.FindFirstCommonChar(
                 "012345678", 1,
                 "_2____2__", 2,
                 out start1, out start2).Should().BeTrue();
             start1.Should().Be(2);
             start2.Should().Be(6);
 
-            TextCompareUtil.FindFirstCommonChar(
+            DiffMergeUtil.FindFirstCommonChar(
                 "012345679", 1,
                 "_2____2__", 7,
                 out start1, out start2).Should().BeFalse();
 
-            TextCompareUtil.FindFirstCommonChar(
+            DiffMergeUtil.FindFirstCommonChar(
                 "012345679", 3,
                 "_2____2__", 1,
                 out start1, out start2).Should().BeFalse();
         }
 
         [Test]
-        public void CommonCharsLength_sould_calc_common_chars_sequence_length()
+        public void CommonCharsLength_should_calc_common_chars_sequence_length()
         {
-            TextCompareUtil.CommonCharsLength(
+            DiffMergeUtil.CommonCharsLength(
                 "0123456", 1,
                 "0123456", 1).Should().Be(6);
 
-            TextCompareUtil.CommonCharsLength(
+            DiffMergeUtil.CommonCharsLength(
                 "0123456789", 0,
                 "01234567cd", 0).Should().Be(8);
 
-            TextCompareUtil.CommonCharsLength(
+            DiffMergeUtil.CommonCharsLength(
                 "0123456789", 2,
                 "01234567cd", 2).Should().Be(6);
 
-            TextCompareUtil.CommonCharsLength(
+            DiffMergeUtil.CommonCharsLength(
                 "0123456789", 7,
                 "01234567cd", 7).Should().Be(1);
-        }
-    }
-
-    public class Replace
-    {
-    }
-
-    public class Diff
-    {
-        public IEnumerable<Replace> Replaces { get; private set; }
-    }
-
-    public static class DiffMergeUtil
-    {
-        public static Diff MakeDiff(string text1, string text2)
-        {
-            var positions = TextCompareUtil.CommonSubstrings(text1, text2);
-            return MakeDiff(positions, text1.Length, text2.Length);
-        }
-
-        public static Diff MakeDiff(IEnumerable<ISubstringPosition> substringPositions, int firstLength, int secondLength)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    [TestFixture]
-    public class DiffMergeUtilTests
-    {
-        private class PositionForTesting : ISubstringPosition
-        {
-            public static IEnumerable<ISubstringPosition> Build(params PositionForTesting[] positions)
-            {
-                return positions;
-            }
-
-            public int FirstStart { get; set; }
-
-            public int SecondStart { get; set; }
-
-            public string Value { get; set; }
-        }
-
-        [Test]
-        public void MakeDiff_should_make_diff()
-        {
-            throw new NotImplementedException();
-
-            var diff1 = DiffMergeUtil.MakeDiff(
-                PositionForTesting.Build(
-                    new PositionForTesting {FirstStart = 1, SecondStart = 2, Value = "abc"}),
-                10, 10);
-            diff1.Replaces.Should().HaveCount(2);
-            
         }
     }
 }
